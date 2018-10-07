c55d8ec56ffd5930b5526f168690d9f8e90f8e74
newmastercode <marcinwarzybok@outlook.com>
2018-10-07 12:02:44 +0200

    working to left

--- js/main.js
+++ js/main.js
@@ -2,17 +2,27 @@ let leftArrow = document.getElementById('left-arrow')
 let rightArrow = document.getElementById('right-arrow')
 let filmsList = document.querySelector('.films-list')
 let filmItems = document.querySelectorAll('.film-item')
+let move = 1
 
-window.onresize = (event) => {
+onResize()
+
+function onResize(event) {
     for (let i = 0; i < filmItems.length; i++) {
         console.log(filmsList.clientWidth)
         console.log(i * 170)
-        if(i * 180 > filmsList.clientWidth) {
-            filmItems.item(i).style.display = "none"
-        }else{
-            filmItems.item(i).style.left = i * 180 + 'px'
-        }
+        filmItems.item(i).style.left = i * 180 + 'px'
+        // if (i * 180 > filmsList.clientWidth) {
+        //     // filmItems.item(i).style.display = "none"
+        // } else {
+
+        // }
     }
+    filmItems.item(filmItems.length - 1).style.left = "-180px"
+    // filmItems.item(filmItems.length - 1).style.display = "block"
+}
+
+window.onresize = (event) => {
+    onResize(event)
 }
 
 
@@ -21,8 +31,20 @@ leftArrow.addEventListener("click", () => {
     // // filmsList.style.left = (filmsList.offsetLeft + 30).toString() + "px"
     // filmsList.children.item(0).parentNode.insertBefore(filmsList.children.item(filmsList.children.length - 1),filmsList.children.item(0))
     // // filmsList.children.item(filmsList.children.length - 1).remove()
+    for (let i = 0; i < filmItems.length; i++) {
+        let item = filmItems.item(i)
+        if (i === filmItems.length - move) {
+            item.style.display = "none"
+            item.style.left = "-180px"
+            item.style.display = "block"
+            move += 1
+            if (move === filmItems.length) {
+                move = 1
+            }
+        }
+        item.style.left = (parseInt(item.style.left) + 180) + 'px'
+    }
 
-    
 
 })
 
